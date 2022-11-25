76b24d487fa7c23514f4ac309ff682180df3acd7
Virufighter <alvinpoulose4@gmail.com>
2022-11-25 12:24:28 +0530

    view

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { AddTodoComponent } from './add-todo/add-todo.component';
+import { ViewAllTodoComponent } from './view-all-todo/view-all-todo.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    AddTodoComponent
+    AddTodoComponent,
+    ViewAllTodoComponent
   ],
   imports: [
     BrowserModule,
